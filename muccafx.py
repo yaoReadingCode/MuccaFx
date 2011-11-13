e8f2e46723a24fea0b7bd462b394a84195f73c8a
IlMattoDel93 <mattia.manzati@gmail.com>
2011-11-13 15:22:19 +0100

    Started assMeta, assStyle, assFile
    Signed-off-by: IlMattoDel93 <mattia.manzati@gmail.com>

--- muccafx.py
+++ muccafx.py
@@ -4,6 +4,33 @@ import win32ui
 import re
 
 class assStyle:
+    fontname = 'Arial'
+    fontsize = 20
+    alpha1 = '&H00'
+    color1 = '&H00FFFFFF'
+    alpha2 = '&H00'
+    color2 = '&H000000FF'
+    alpha3 = '&H00'
+    color3 = '&H00000000'
+    alpha4 = '&H00'
+    color4 = '&H00000000'
+    bold = 0
+    italic = 0
+    underline = 0
+    strikeout = 0
+    scale_x = 100
+    scale_y = 100
+    spacing = 0
+    angle = 0
+    borderstyle = 1
+    outline = 2
+    shadow = 2
+    alignment = 2
+    margin_l = 10
+    margin_r = 10
+    margin_v = 10
+    encoding = 1
+    
     def __init__(self, data = {}):
         if 'fontname' in data:
             self.fontname = str(data['fontname'])
@@ -27,7 +54,38 @@ class assStyle:
             self.color4 = str(data['color4'])
         if 'bold' in data:
             self.bold = int(data['bold'])
-    
+        if 'italic' in data:
+            self.italic = int(data['italic'])
+        if 'underline' in data:
+            self.underline = int(data['underline'])
+        if 'strikeout' in data:
+            self.strikeout = int(data['strikeout'])
+        if 'scale_x' in data:
+            self.scale_x = float(data['scale_x'])
+        if 'scale_y' in data:
+            self.scale_y = float(data['scale_y'])
+        if 'spacing' in data:
+            self.spacing = float(data['spacing'])
+        if 'angle' in data:
+            self.angle = float(data['angle'])
+        if 'borderstyle' in data:
+            self.borderstyle = int(data['borderstyle'])
+        if 'outline' in data:
+            self.outline = float(data['outline'])
+        if 'shadow' in data:
+            self.shadow = float(data['shadow'])
+        if 'alignment' in data:
+            self.alignment = int(data['alignment'])
+        if 'margin_l' in data:
+            self.margin_l = int(data['margin_l'])
+        if 'margin_r' in data:
+            self.margin_r = int(data['margin_r'])
+        if 'margin_v' in data:
+            self.margin_v = int(data['margin_v'])
+        if 'encoding' in data:
+            self.encoding = int(data['encoding'])
+        pass
+
 class assLine:
     text = ''
     start_time = 0
@@ -54,6 +112,50 @@ class assLine:
                 self.text += str(syl)
         return re.sub('(\\{.*?\\})','',str(self.text))
 
+class assMeta:
+    name = ''
+    value = ''
+
+    def __init__(self, data = {}):
+        if 'name' in data:
+            self.name = data['name']
+        if 'value' in data:
+            self.value = data['value']
+        pass
+
+class assFile:
+    lines = []
+    styles = []
+    metas = []
+    path = None
+
+    def __init__(self, path = None):
+        self.path = path
+
+    def open(self, path = None):
+        if path == None:
+            path = self.path
+        if path == None:
+            return None
+        try:
+            fp = open(self.path, 'rb+')
+        except:
+            return None
+        for line in iter(fp):
+            print "|{0}|".format(line)
+        fp.close()
+        
+
+    def appendLine(self, line):
+        self.lines.append(line)
+
+    def appendStyle(self, style):
+        self.styles.append(style)
+
+    def appendMeta(self, meta):
+        self.metas.append(meta)
+    
+
 class MuccaFxEffect:
     ass = None
     nlines = []
@@ -69,64 +171,38 @@ class MuccaFxEffect:
         return self.nlines
 
 class TextToy:
-    def __init__(self, text = '', style = {}):
-        self.style = {
-            'fontname':'Arial',
-            'fontsize':20,
-            'alpha1':'&H00',
-            'color1':'&H00FFFFFF',
-            'alpha2':'&H00',
-            'color2':'&H000000FF',
-            'alpha3':'&H00',
-            'color3':'&H00000000',
-            'alpha4':'&H00',
-            'color4':'&H00000000',
-            'bold':0,
-            'italic':0,
-            'underline':0,
-            'strikeout':0,
-            'scale_x':100,
-            'scale_y':100,
-            'spacing':0,
-            'angle':0,
-            'borderstyle':1,
-            'outline':2,
-            'shadow':2,
-            'alignment':2,
-            'margin_l':10,
-            'margin_r':10,
-            'margin_v':10,
-            'encoding':1
-        }
-        self.text = text
-        self.style.update(style)
+    def __init__(self, text = '', style = None):
+        if style == None:
+            style = assStyle()
+        self.text = str(text)
+        self.style = style
         pass
 
     def selectFont(self, dc, style = None, scale = 64):
         if style == None:
             style = self.style
         font = win32gui.LOGFONT()
-        font.lfHeight = style['fontsize'] * scale
-        if style['bold']:
+        font.lfHeight = style.fontsize * scale
+        if style.bold:
             font.lfWeight = win32con.FW_BOLD
         else:
             font.lfWeight = win32con.FW_NORMAL
-        font.lfItalic = style['italic']
-        font.lfUnderline = style['underline']
-        font.lfStrikeOut = style['strikeout']
-        font.lfCharSet = style['encoding']
+        font.lfItalic = style.italic
+        font.lfUnderline = style.underline
+        font.lfStrikeOut = style.strikeout
+        font.lfCharSet = style.encoding
         font.lfOutPrecision = win32con.OUT_TT_PRECIS;
         font.lfClipPrecision = win32con.CLIP_DEFAULT_PRECIS;
         font.lfQuality = win32con.ANTIALIASED_QUALITY;
         font.lfPitchAndFamily = win32con.DEFAULT_PITCH | win32con.FF_DONTCARE;
-        font.lfFaceName = style['fontname'][:32]
+        font.lfFaceName = style.fontname[:32]
         font = win32gui.CreateFontIndirect(font)
         win32gui.SelectObject(dc, font)
         pass
 
     def getExtents(self, text = None, style = None):
         if text == None:
-            text = self.text
+            text = str(self.text)
         if style == None:
             style = self.style
         txtext = {}
@@ -134,10 +210,10 @@ class TextToy:
         win32gui.SetMapMode(dc, win32con.MM_TEXT)
         font = self.selectFont(dc, style, 64)
 
-        if style['spacing']:
+        if style.spacing:
             for char in text:
                 cx, cy = win32gui.GetTextExtentPoint32(dc, char)
-                txtext['width'] += cx + (style['spacing'] * 64)
+                txtext['width'] += cx + (style.spacing * 64)
                 txtext['height'] = cy
         else:
             cx, cy = win32gui.GetTextExtentPoint32(dc, text)
@@ -150,16 +226,18 @@ class TextToy:
         txtext['internal_lead'] = tm['InternalLeading'];
         txtext['external_lead'] = tm['ExternalLeading'];
 
-        txtext['width'] = txtext['width'] * (style['scale_x'] / float(100)) / float(64);
-        txtext['height'] = txtext['height'] * (style['scale_y'] / float(100)) / float(64);
-        txtext['ascent'] = txtext['ascent'] * (style['scale_y'] / float(100)) / float(64);
-        txtext['descent'] = txtext['descent'] * (style['scale_y'] / float(100)) / float(64);
-        txtext['internal_lead'] = txtext['internal_lead'] * (style['scale_y'] / float(100)) / float(64);
-        txtext['external_lead'] = txtext['external_lead'] * (style['scale_y'] / float(100)) / float(64);
+        txtext['width'] = txtext['width'] * (style.scale_x / float(100)) / float(64);
+        txtext['height'] = txtext['height'] * (style.scale_y / float(100)) / float(64);
+        txtext['ascent'] = txtext['ascent'] * (style.scale_y / float(100)) / float(64);
+        txtext['descent'] = txtext['descent'] * (style.scale_y / float(100)) / float(64);
+        txtext['internal_lead'] = txtext['internal_lead'] * (style.scale_y / float(100)) / float(64);
+        txtext['external_lead'] = txtext['external_lead'] * (style.scale_y / float(100)) / float(64);
         return txtext
         
 
 if __name__ == "__main__":
-    print TextToy("T",{'italic':True}).getExtents()
-    line = FxLine({'text':'{\b1}LOL{\b0}'})
+    print TextToy("T").getExtents()
+    line = assLine({'text':'{\b1}LOL{\b0}'})
     print line
+    f = assFile("preview.ass")
+    f.open()
